3f9e5709178f93e639c768d890759cfac8086aca
kimura7150 <rcpd0715@gmail.com>
2015-07-03 21:20:50 -0400

    Pet Changes- Pet Power

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -116,6 +116,9 @@ RULE_INT(Character, TradeskillUpResearch, 1) // Research skillup rate adjust. Lo
 RULE_INT(Character, TradeskillUpTinkering, 2) // Tinkering skillup rate adjust. Lower is faster.
 RULE_BOOL(Character, SpamHPUpdates, false) // if your server has stupid amounts of HP that causes client display issues, turn this on!
 RULE_BOOL(Character, MarqueeHPUpdates, false) // Will show Health % in center of screen < 100%
+RULE_REAL(Character, PetPowerActPower, 50.0f)
+RULE_INT(Character, PetPowerMinDmg, 2)
+RULE_INT(Character, PetPowerMaxDmg, 20)
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Mercs)
